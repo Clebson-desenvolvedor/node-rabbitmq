f84f842faee809e5eca18b031502882db779ed3c
clebson.araujo <clebson.araujo@l5.com.br>
2025-08-23 09:40:40 -0300

    Correções no .env, refatoração do config.ts e consumer.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,17 +3,17 @@ dotenv.config();
 
 export default {
     rabbitmq: {
-        url: process.env.RABBITMQ_URL || "amqp://guest@rabbitmq:5672",
-        queue: process.env.RABBITMQ_QUEUE || "orders_queue",
-        dql: process.env.RABBITMQ_DLQ || "orders_dlq"
+        url: process.env.RABBITMQ_URL!,
+        queue: process.env.RABBITMQ_QUEUE!,
+        dlq: process.env.RABBITMQ_DLQ!
     },
     batch: {
-        size: parseInt(process.env.BATCH_SIZE ?? "") || 10,
-        interval: parseInt(process.env.BATCH_INTERVAL ?? "")
+        size: parseInt(process.env.BATCH_SIZE ?? "")!,
+        interval: parseInt(process.env.BATCH_INTERVAL ?? "")!
     },
     retry: {
-        maxAttempts: parseInt(process.env.RETRY_MAX_ATTEMPTS ?? "") || 3,
-        delay: parseInt(process.env.RETRY_DELAY ?? "") || 2000
+        maxAttempts: parseInt(process.env.RETRY_MAX_ATTEMPTS ?? "")!,
+        delay: parseInt(process.env.RETRY_DELAY ?? "") || 5000
     },
     db: {
         url: process.env.DB_URL || "postgres://user:pass@postgress:5432/mydb"
--- src/modules/events/consumer.ts
+++ src/modules/events/consumer.ts
@@ -1,4 +1,4 @@
-import amqp from "amqplib"
+import amqp from "amqplib";
 import config from "../../config/config";
 import processEvent from "./eventService";
 import logger from "./logger";
@@ -7,38 +7,45 @@ const rabbitmq = config.rabbitmq;
 const retry = config.retry;
 
 export default async function startConsumer(): Promise<void> {
-    const connection = await amqp.connect(rabbitmq.url);
-    const channel = await connection.createChannel();
+    let connection;
+    let attempts = 0;
 
+    while (!connection && attempts < retry.maxAttempts) {
+        try {
+            connection = await amqp.connect(rabbitmq.url);
+        } catch (error: any) {
+            attempts++;
+            logger.warn(`Tentativa ${attempts} de conexão com RabbitMQ falhou: ${error.message}`);
+            await new Promise((res) => setTimeout(res, retry.delay));
+        }
+    }
+
+    if (!connection) throw new Error("Não foi possível conectar ao RabbitMQ após múltiplas tentativas.");
+
+    const channel = await connection.createChannel();
     await channel.assertQueue(rabbitmq.queue, { durable: true });
-    await channel.assertQueue(rabbitmq.dql, { durable: true });
+    await channel.assertQueue(rabbitmq.dlq, { durable: true });
 
     channel.consume(rabbitmq.queue, async (msg) => {
         if (msg != null) {
             const event = JSON.parse(msg.content.toString());
-
             let attempts = 0;
 
             while (attempts < retry.maxAttempts) {
                 try {
                     await processEvent(event);
-
                     channel.ack(msg);
-
                     return;
                 } catch (error: any) {
                     attempts++;
-
                     logger.error(`Erro processando evento. Tentativa ${attempts}: ${error.message}`);
-
-                    await new Promise(r => setTimeout(r, retry.delay));
+                    await new Promise((r) => setTimeout(r, retry.delay));
                 }
-
-                channel.sendToQueue(rabbitmq.dql, Buffer.from(JSON.stringify(event)));
-                channel.ack(msg);
-
-                logger.warn(`Mensagem enviada para DLQ: ${rabbitmq.dql}`);
             }
+
+            channel.sendToQueue(rabbitmq.dlq, Buffer.from(JSON.stringify(event)));
+            channel.ack(msg);
+            logger.warn(`Mensagem enviada para DLQ: ${rabbitmq.dlq}`);
         }
     });
 }
